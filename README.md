68f32054d8807d32c3da68efed83cb68cd385f66
Dan Sullivan <dansullivan@gmail.com>
2016-12-10 11:19:51 -0600

    got streaming example working from https://github.com/dghubble/go-twitter/tree/master/examples

--- README.md
+++ README.md
@@ -8,10 +8,35 @@ Install Go
 
 `brew install go`
 
-Clone It
+Setup a Go environment
+```
+mkdir -p ~/working/github/go/src/
+echo 'export GOPATH=~/working/github/go' >> ~/.bashrc
+echo 'export PATH=$PATH:~/working/github/go/bin' >> ~/.bashrc
+export PATH=$PATH:~/working/github/go/bin
+```
 
-`git clone git@github.com:dsulli99/gorunning.git`
+Clone it
+```
+cd ~/working/github/go/src/
+git clone git@github.com:dsulli99/gorunning.git
+```
 
-Start It
+Start it
+```
+export TWITTER_CONSUMER_KEY=xxx
+export TWITTER_CONSUMER_SECRET=xxx
+export TWITTER_ACCESS_TOKEN=xxx
+export TWITTER_ACCESS_SECRET=xxx
+go run gorunning.go
+```
 
-`go run gorunning.go`
+Getting help
+`go run gorunning.go --help`
+
+# Godep notes 
+
+Install godep
+```
+go get github.com/tools/godep
+```
--- gorunning.go
+++ gorunning.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-  "io"
   "flag"
-  "net/http"
-  "os"
+  "fmt"
   "log"
+  "os"
+  "os/signal"
+  "syscall"
 
   "github.com/coreos/pkg/flagutil"
-  //"github.com/dghubble/go-twitter/twitter"
-  //"github.com/dghubble/oauth1"
-
+  "github.com/dghubble/go-twitter/twitter"
+  "github.com/dghubble/oauth1"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-  io.WriteString(w, "Hello world!")
-}
+//func hello(w http.ResponseWriter, r *http.Request) {
+//  io.WriteString(w, "Hello world!")
+//}
 
 func main() {
   //http.HandleFunc("/", hello)
@@ -32,5 +32,70 @@ func main() {
   if *consumerKey == "" || *consumerSecret == "" || *accessToken == "" || *accessSecret == "" {
     log.Fatal("Consumer key/secret and Access token/secret required")
   } 
+  
+  fmt.Println("All requirements met...")
+  
+  config := oauth1.NewConfig(*consumerKey, *consumerSecret)
+  token := oauth1.NewToken(*accessToken, *accessSecret)
+  // OAuth1 http.Client will automatically authorize Requests
+  httpClient := config.Client(oauth1.NoContext, token)
 
+  // Twitter Client
+  client := twitter.NewClient(httpClient)
+
+  // Convenience Demux demultiplexed stream messages
+  demux := twitter.NewSwitchDemux()
+  demux.Tweet = func(tweet *twitter.Tweet) {
+    fmt.Println(tweet.Text)
+  }
+  demux.DM = func(dm *twitter.DirectMessage) {
+    fmt.Println(dm.SenderID)
+  }
+  demux.Event = func(event *twitter.Event) {
+    fmt.Printf("%#v\n", event)
+  }
+
+  fmt.Println("Starting Stream...")
+
+  // FILTER
+  filterParams := &twitter.StreamFilterParams{
+    Track:         []string{"cat"},
+    StallWarnings: twitter.Bool(true),
+  }
+  stream, err := client.Streams.Filter(filterParams)
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  // USER (quick test: auth'd user likes a tweet -> event)
+  // userParams := &twitter.StreamUserParams{
+  //  StallWarnings: twitter.Bool(true),
+  //  With:          "followings",
+  //  Language:      []string{"en"},
+  // }
+  // stream, err := client.Streams.User(userParams)
+  // if err != nil {
+  //  log.Fatal(err)
+  // }
+
+  // SAMPLE
+  // sampleParams := &twitter.StreamSampleParams{
+  //  StallWarnings: twitter.Bool(true),
+  // }
+  // stream, err := client.Streams.Sample(sampleParams)
+  // if err != nil {
+  //  log.Fatal(err)
+  // }
+
+  // Receive messages until stopped or stream quits
+  go demux.HandleChan(stream.Messages)
+
+  // Wait for SIGINT and SIGTERM (HIT CTRL-C)
+  ch := make(chan os.Signal)
+  signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+  log.Println(<-ch)
+
+  fmt.Println("Stopping Stream...")
+  stream.Stop()
+   
 }
